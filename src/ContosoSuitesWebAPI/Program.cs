c43452970049350e26f5b4a6076014ac1988e826
gitName <a@b.c>
2024-11-07 17:02:03 +0800

    day Ex3

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -60,29 +60,32 @@ app.MapGet("/", async () =>
     .WithName("Index")
     .WithOpenApi();
 
-// Retrieve the set of hotels from the database.
-app.MapGet("/Hotels", async () => 
-{
-    throw new NotImplementedException();
-})
-    .WithName("GetHotels")
-    .WithOpenApi();
+ // Retrieve the set of hotels from the database.
+ app.MapGet("/Hotels", async () => 
+ {
+     var hotels = await app.Services.GetRequiredService<IDatabaseService>().GetHotels();
+     return hotels;
+ })
+     .WithName("GetHotels")
+     .WithOpenApi();
 
-// Retrieve the bookings for a specific hotel.
-app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
-{
-    throw new NotImplementedException();
-})
-    .WithName("GetBookingsForHotel")
-    .WithOpenApi();
+ // Retrieve the bookings for a specific hotel.
+ app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
+ {
+     var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsForHotel(hotelId);
+     return bookings;
+ })
+     .WithName("GetBookingsForHotel")
+     .WithOpenApi();
 
-// Retrieve the bookings for a specific hotel that are after a specified date.
-app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
-{
-    throw new NotImplementedException();
-})
-    .WithName("GetRecentBookingsForHotel")
-    .WithOpenApi();
+ // Retrieve the bookings for a specific hotel that are after a specified date.
+ app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
+ {
+     var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsByHotelAndMinimumDate(hotelId, min_date);
+     return bookings;
+ })
+     .WithName("GetRecentBookingsForHotel")
+     .WithOpenApi();
 
 // This endpoint is used to send a message to the Azure OpenAI endpoint.
 app.MapPost("/Chat", async Task<string> (HttpRequest request) =>
